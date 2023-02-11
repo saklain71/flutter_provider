3dbf976c19f05653a9d9a02e803df023f617a8d6
Saklain Mustak <saklainm71@gmail.com>
2023-02-11 12:20:25 +0600

    provider implemented

--- lib/MyHome.dart
+++ lib/MyHome.dart
@@ -20,42 +20,82 @@ class _MyHomePageState extends State<MyHome> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Consumer<UserDetailsProvider>(
-        builder: (context, provider, child) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Hi' + provider.userName,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<UserDetailsProvider>(
+              builder: (context, provider, child) {
+                return Center(
+                  child: Column(
+                  //  mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Hi ' + provider.userName,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 15,),
+                      Text(
+                        'You are ' + provider.userAge.toString() + ' years old',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(onPressed: (){
+                            provider.increseAge();
+                          }, child: Text('Increase Age',
+                            style: TextStyle(color: Colors.green),)),
+                          TextButton(onPressed: (){
+                            provider.decreaseAge();
+                          }, child: Text('Decrease Age',
+                              style: TextStyle(color: Colors.red))),
+                        ],),
+                      Text(
+                        'Hi ' + provider.userName2,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 15,),
+                      Text(
+                        'You are ' + provider.userAge2.toString() + ' years old',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(onPressed: (){
+                            provider.increseAge();
+                          }, child: Text('Increase Age',
+                            style: TextStyle(color: Colors.green),)),
+                          TextButton(onPressed: (){
+                            provider.decreaseAge();
+                          }, child: Text('Decrease Age',
+                              style: TextStyle(color: Colors.red))),
+                        ],),
+                      
+                      TextButton(onPressed: (){
+                        provider.seeResult(context);
+                      }, child: Text("See Result",style: TextStyle(fontSize: 30),))
+
+                    ],
                   ),
-                ),
-                Text(
-                  'You are ' + provider.userAge.toString() + ' years old',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                 TextButton(onPressed: (){
-                   provider.increseAge();
-                 }, child: Text('Increase Age',
-                 style: TextStyle(color: Colors.green),)),
-                 TextButton(onPressed: (){
-                   provider.decreaseAge();
-                 }, child: Text('Decrease Age',
-                     style: TextStyle(color: Colors.red))),
-               ],)
-              ],
+                );
+              },
             ),
-          );
-        },
+          ],
+        ),
       ),
 
     );
--- lib/UserDetailsProvider.dart
+++ lib/UserDetailsProvider.dart
@@ -1,13 +1,19 @@
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UserDetailsProvider extends ChangeNotifier{
 
   TextEditingController  nameController  = TextEditingController();
   TextEditingController  ageController  = TextEditingController();
-  int _age = 0;
-  String _userName = "";
+  int _age = 27;
+  int _age2 = 28;
+  String _userName = "Sami";
+  String _userName2 = "Tithi";
   int get userAge => _age;
+  int get userAge2 => _age2;
   String get userName => _userName;
+  String get userName2 => _userName2;
 
 void updateAge (int age){
   _age = age;
@@ -22,9 +28,22 @@ void updateAge (int age){
     _age -= 1;
     notifyListeners();
   }
-  void updateName(String name) {
+  void userNameSami(String name) {
     _userName = name;
     notifyListeners();
+  }
+  void userNameTithi(String name) {
+    _userName2 = name;
+    notifyListeners();
+  }
+  void seeResult(context){
+   ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: const Duration(seconds: 5),
+            content: Text('OPPS!!! Get Married Early!!!',
+              style: TextStyle(color: Colors.red),
+            )));
+   notifyListeners();
   }
 
 }
